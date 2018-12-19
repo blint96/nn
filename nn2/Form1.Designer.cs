a0de32e298b7354cc9df58c2d5bfb39f1ed53b2b
blint96 <sobiec1996@gmail.com>
2018-12-19 19:06:51 +0100

    Add project files.

---
+++ nn2/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace nn2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(629, 27);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(159, 33);
+            this.btnClear.TabIndex = 0;
+            this.btnClear.Text = "Wyczyść";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(629, 66);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(159, 33);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Zapisz";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(629, 105);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(159, 28);
+            this.txtName.TabIndex = 2;
+            this.txtName.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 570);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnClear);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.RichTextBox txtName;
+    }
+}
+
---
+++ nn2/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nn2
+{
+    public partial class Form1 : Form
+    {
+        private List<PictureBox> inputs = new List<PictureBox>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startMatrix()
+        {
+            if(inputs.Count > 0)
+            {
+                foreach(PictureBox item in inputs) {
+                    this.Controls.Remove(item);
+                }
+            }
+
+            inputs.Clear();
+
+            int top = 42;
+            int left = 10;
+            for (int i = 0; i < 36; i++)
+            {
+                PictureBox tmp = new PictureBox();
+                tmp.Height = 32;
+                tmp.Width = 32;
+                tmp.Left = left;
+                tmp.Top = top;
+                tmp.BackColor = Color.White;
+                tmp.Name = "ipt_" + i;
+                this.Controls.Add(tmp);
+                inputs.Add(tmp);
+
+                // "click" element
+                tmp.Click += (s, err) => {
+                    if (tmp.BackColor == Color.White)
+                        tmp.BackColor = Color.Black;
+                    else
+                        tmp.BackColor = Color.White;
+                };
+
+                left += 38;
+                if (((i + 1) % 6) == 0)
+                {
+                    top = top + 38;
+                    left = 10;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            startMatrix();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            startMatrix();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            String patternName = txtName.Text;
+            if(patternName.Length == 0)
+            {
+                MessageBox.Show("Musisz wprowadzić symbol jakiemu odpowiada wzór, który narysowałeś/aś.");
+                return;
+            }
+        }
+    }
+}
